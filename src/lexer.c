76f2b45c8c57633f43ebd1328cc08cb3b61de864
Matthew Knowles <mattknowles314@proton.me>
2024-08-04 14:14:25 +0100

    add isKeyword

--- src/lexer.c
+++ src/lexer.c
@@ -21,6 +21,22 @@ bool isOperatior(char chr) {
             chr == '=');
 }
 
+bool isKeyword(char* str) {
+    const char* keywords[] = {
+        "int",
+        "float",
+        "str",
+        "if",
+        "elif"
+    };
+    for (int i = 0; i < sizeof(keywords)/sizeof(keywords[0]); i++) {
+        if (strcmp(str, keywords[i] == 0)) {
+                return true;
+        }
+    }
+    return false;
+}
+
 int main(){
     return(1);
 }
